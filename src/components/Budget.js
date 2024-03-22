bd3eb5f2c4dff83a071776528f6cde0927fa8cd0
Xiaozhen2022 <xiaozhen@gmail.com>
2024-03-22 16:21:25 -0400

    final

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,29 +1,45 @@
 import React, { useContext, useState } from 'react';
-import { AppContext } from '../context/AppContext';
+import { AppContext  } from '../context/AppContext';
+
 
 const Budget = () => {
+    const { dispatch, currency,expenses} = useContext(AppContext);
     const { budget } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
-    const handleBudgetChange = (event) => {
-  
-        const inputvalue = event.target.value;
-
-        if (parseInt(inputvalue)>20000) {
-            alert("Your input number is exceed 2000");
-            setNewBudget('2000');
-        }
-        // else if (parseInt(inputvalue)<totalExpenses) {
-        //     alert("Your input number should not less than totalExpenses!");
-        //     setNewBudget('2000');
-        //             }
-        else {setNewBudget(inputvalue);}
-        
+    const upperLimitValue = 20000
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total += item.cost);
+    }, 0);
+    const handleBudgetChange = (e) => {
+        setNewBudget(e.target.value);
     }
+    const handleKeyDown = (e) => {
+
+        if ( e.key === 'Enter' && newBudget <= upperLimitValue && newBudget< totalExpenses) {
+          dispatch({ type: "SET_BUDGET", payload: newBudget });
+        }else if (newBudget > upperLimitValue) {
+            alert("The value cannot exceed upper limit value!");  
+        }
+         else if ( newBudget < totalExpenses) {alert("The value cannot be lower than spending"); }
+
+      };
+      
+
     return (
-<div className='alert alert-secondary'>
-<span>Budget: £</span>
-<input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
-</div>
+        <div className="alert alert-secondary ">
+            <label>Budget: {currency}</label>
+            <input
+                className='w-50 ms-1'
+                required
+                type="number"
+                id="cost"
+                step={10}
+                value={newBudget}
+                onChange={handleBudgetChange}
+                onKeyDown={handleKeyDown}
+            />
+    </div>
+
     );
 };
 export default Budget;
