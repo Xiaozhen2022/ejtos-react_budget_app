ac537df80f023f1291b45b30bd28d34743620ac6
Xiaozhen2022 <xiaozhen@gmail.com>
2024-03-21 22:34:58 -0400

    tobe edieted

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -10,8 +10,12 @@ const AllocationForm = (props) => {
     const [action, setAction] = useState('');
 
     const submitEvent = () => {
-
-            if(cost > remaining) {
+            if (isNaN(cost)) {
+                alert("Please input number!");
+                setCost("");
+                return;
+            }
+            else if (cost > remaining) {
                 alert("The value cannot exceed remaining funds  £"+remaining);
                 setCost("");
                 return;
@@ -59,7 +63,7 @@ const AllocationForm = (props) => {
                         <option defaultValue value="Add" name="Add">Add</option>
                 <option value="Reduce" name="Reduce">Reduce</option>
                   </select>
-
+                   <span>  £</span>
                     <input
                         required='required'
                         type='number'
--- src/components/Budget.js
+++ src/components/Budget.js
@@ -5,11 +5,23 @@ const Budget = () => {
     const { budget } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
     const handleBudgetChange = (event) => {
-        setNewBudget(event.target.value);
+  
+        const inputvalue = event.target.value;
+
+        if (parseInt(inputvalue)>20000) {
+            alert("Your input number is exceed 2000");
+            setNewBudget('2000');
+        }
+        // else if (parseInt(inputvalue)<totalExpenses) {
+        //     alert("Your input number should not less than totalExpenses!");
+        //     setNewBudget('2000');
+        //             }
+        else {setNewBudget(inputvalue);}
+        
     }
     return (
 <div className='alert alert-secondary'>
-<span>Budget: £{budget}</span>
+<span>Budget: £</span>
 <input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
 </div>
     );
